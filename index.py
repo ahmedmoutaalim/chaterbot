2003f2e7218533b32efc85376e8d2afc9b3b97fa
Omar <57999049+ahmedmoutaalim@users.noreply.github.com>
2023-01-04 20:00:15 +0100

    add chatterbot code

--- index.py
+++ index.py
@@ -1,15 +1,30 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-bot = ChatBot("chatbot" , read_only=False , logic_adapters=["chatterbot.logic.BestMatch"])
+bot = ChatBot("chatbot" , read_only=False,
+ logic_adapters=[
+    {
+        "import_path" : "chatterbot.logic.BestMatch",
+        "default_response" : "I'm sorry, but I'm unable to find an answer to your question.",
+        "maximum_similarity_threshold" : 0.9  
+        }])
 
 list_to_train = [
-    "",
-    "",
-    "",
+    "hi",
+    "hi there",
+    "what mean chaimaa to you ?",
+    "she's my lobster my heart my soulmate she's anything to me ♡ "
 ]
 
 List_trainer = ListTrainer(bot)
 
-list_train.train()
+List_trainer.train(list_to_train)
+
+while True :
+      user_response = input("user: ")
+      print("Chatbot: " + str(bot.get_response(user_response)))
+
+
+
+
 
